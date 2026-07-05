c25b840fe5861251d7543aee67642730d3e296f1
Manas  Arora <manas.arora@cginfinity.com>
2026-07-06 00:43:05 +0530

    Added GET product by id endpoint

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Catalog.Api.Dtos;
+using Catalog.Api.Models;
+using Catalog.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductRepository _repository;
+
+    public ProductsController(IProductRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetAll()
+    {
+        var products = await _repository.GetAllAsync();
+
+        return Ok(products);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Product>> GetById(int id)
+    {
+        var product = await _repository.GetByIdAsync(id);
+
+        if (product is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(product);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Catalog.Api.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSingleton<IProductRepository, InMemoryProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
