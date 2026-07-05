6d6ee8ea8bd5c6276447dd6cfe72b95733da7764
Manas  Arora <manas.arora@cginfinity.com>
2026-07-06 00:28:37 +0530

    Added product repository interface

---
+++ Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.Api.Models;
+
+namespace Catalog.Api.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAllAsync();
+
+    Task<Product?> GetByIdAsync(int id);
+
+    Task<Product> CreateAsync(Product product);
+
+    Task<bool> UpdateAsync(Product product);
+
+    Task<bool> DeleteAsync(int id);
+}
